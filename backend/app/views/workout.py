a82bcfc966846a719bf01c9eb3181039b2efc020
Jian Chan <jianlorenzo_chan@yahoo.com.ph>
2020-01-30 01:23:05 +0800

    Remove AWS Keys

--- backend/app/views/workout.py
+++ backend/app/views/workout.py
@@ -137,8 +137,8 @@ def create_workout():
         # S3 Bucket
         s3 = boto3.client(
         "s3",
-        aws_access_key_id="AKIAIWHTYSPQ34XT2QFQ",
-        aws_secret_access_key="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"
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+        aws_secret_access_key=os.environ["AWS_SECRET_KEY"]
         )
         bucket_resource = s3
 
